45329bb294919176767d79aa2c8804b76ac86f4f
Khoa Do <khoado.dk@gmail.com>
2019-10-23 13:26:17 -0400

    delete unused files, and minor change to css

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,41 +2,58 @@ import React, { Component } from 'react';
 import './NavBar.scss';
 import { FaBars, FaDev } from 'react-icons/fa';
 
-
 class NavBar extends Component {
-    burgerToggle = () => {
-		let linksEl = document.querySelector('.narrowLinks');
-		if (linksEl.style.display === 'block') {
-			linksEl.style.display = 'none';
-		} else {
-			linksEl.style.display = 'block';
-        }
+  burgerToggle = () => {
+    let linksEl = document.querySelector('.narrowLinks');
+    if (linksEl.style.display === 'block') {
+      linksEl.style.display = 'none';
+    } else {
+      linksEl.style.display = 'block';
     }
-    render() {
-        return (
-            <nav>
-                <div className="navWide">    
-                    <div className="wideDiv">
-                        <FaDev className="dev" title="Khoa Do"/>
-                        <a href="#hero">About Me</a>
-                        <a href="#skills">Skills</a>
-                        <a href="#projects">Projects</a>
-                        <a href="#contact">Contact Me</a>
-                        <a href="https://www.dropbox.com/s/m2xzcbph5noo7en/Resume-10-20.docx?dl=0" target="_blank" rel="noopener noreferrer">Resume</a>
-                    </div>
-                </div>
-                <div className="navNarrow">
-                    <FaBars className="nav-icon" onClick={this.burgerToggle} />
-                    <div className="narrowLinks">
-                        <a href="#hero" onClick={this.burgerToggle}>About Me</a>
-                        <a href="#skills" onClick={this.burgerToggle}>Skills</a>
-                        <a href="#projects" onClick={this.burgerToggle}>Projects</a>
-                        <a href="#contact" onClick={this.burgerToggle}>Contact Me</a>
-                        <a href="https://www.dropbox.com/s/m2xzcbph5noo7en/Resume-10-20.docx?dl=0" target="_blank" rel="noopener noreferrer">Resume</a>
-                    </div>
-                </div>
-            </nav>
-            );
-    }
+  };
+  render() {
+    return (
+      <nav>
+        <div className='navWide'>
+          <div className='wideDiv'>
+            <FaDev className='dev' title='Khoa Do' />
+            <a href='#hero'>About Me</a>
+            <a href='#skills'>Skills</a>
+            <a href='#projects'>Projects</a>
+            <a href='#contact'>Contact Me</a>
+            <a
+              href='https://www.dropbox.com/s/duu4ujo4r35f3gc/Resume-10-20.docx?dl=0'
+              target='_blank'
+              rel='noopener noreferrer'>
+              Resume
+            </a>
+          </div>
+        </div>
+        <div className='navNarrow'>
+          <FaBars className='nav-icon' onClick={this.burgerToggle} />
+          <div className='narrowLinks'>
+            <a href='#hero' onClick={this.burgerToggle}>
+              About Me
+            </a>
+            <a href='#skills' onClick={this.burgerToggle}>
+              Skills
+            </a>
+            <a href='#projects' onClick={this.burgerToggle}>
+              Projects
+            </a>
+            <a href='#contact' onClick={this.burgerToggle}>
+              Contact Me
+            </a>
+            <a
+              href='https://www.dropbox.com/s/duu4ujo4r35f3gc/Resume-10-20.docx?dl=0'
+              target='_blank'
+              rel='noopener noreferrer'>
+              Resume
+            </a>
+          </div>
+        </div>
+      </nav>
+    );
+  }
 }
-export default NavBar
+export default NavBar;
