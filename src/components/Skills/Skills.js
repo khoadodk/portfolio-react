b1dcb9ee24e6d69f0a477482029b27a947a4da32
Khoa Do <khoado.dk@gmail.com>
2019-10-30 20:38:24 -0400

    added a project and update resume link

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,50 +1,57 @@
 import React from 'react';
 import './Skills.scss';
-import { FaHtml5, FaCss3, FaJs, FaReact, FaNode } from "react-icons/fa";
-
+import { FaHtml5, FaCss3, FaJs, FaReact, FaNode } from 'react-icons/fa';
 
 const Skills = () => {
-    return (
-        <section id="skills">
-            <div className="heading">
-                <h1>MY PRINCIPLES</h1>
-                <div className="card-container">
-                    <div className="card" >
-                        <h4>foundation</h4>
-                        <p>We don't use algorithms and data structures much at work. But I think understanding them will make us the better at problem solving. 
-                            I focus on learning the fundamentals of one language and be the master of it.
-                        </p>
-                    </div>
-                    <div className="card" >
-                        <h4>clean, maintainable, scalable code</h4>
-                        <p>Clean code will reduce frequency of bugs, long-term costs of maintenance 
-                            and increase collaboration and productivity. So my aim is to write clean, simple, and easy to read code.
-                        </p>
-                    </div>
-                    <div className="card" >
-                        <h4>always learning</h4>
-                        <p>The Imposter Syndrome is real and it is for the better good. 
-                        The web technology is growing exponentially fast, thus I learn to learn and thrive the be the best learner in the room at any given time.   
-                        </p>                    
-                    </div>
-                </div>
-            </div>
+  return (
+    <section id='skills'>
+      <div className='heading'>
+        <h1>MY PRINCIPLES</h1>
+        <div className='card-container'>
+          <div className='card'>
+            <h4>foundation</h4>
+            <p>
+              We don't use algorithms and data structures much at work. But I
+              think understanding them will make us the better at problem
+              solving. I focus on learning the fundamentals of one language and
+              be the master of it.
+            </p>
+          </div>
+          <div className='card'>
+            <h4>clean, maintainable, scalable code</h4>
+            <p>
+              Clean code will reduce frequency of bugs, long-term costs of
+              maintenance and increase collaboration and productivity. So my aim
+              is to write clean, simple, and easy to read code.
+            </p>
+          </div>
+          <div className='card'>
+            <h4>always be learning</h4>
+            <p>
+              Programming is about 80% of studying and 20% of writing code. The
+              web technology is growing fast, thus I learn to learn and thrive
+              the be the best learner in the room at any given time.
+            </p>
+          </div>
+        </div>
+      </div>
 
-            <div className="quote">
-                <q>The future belongs to those who learn 
-                    more skills and combine them in creative ways 
-                    --- Robert Greene</q>
-            </div>
-            <div className="skills-icons">
-                <FaHtml5 className="icon" title="HTML5"></FaHtml5>
-                <FaCss3 className="icon" title="CSS3" />
-                <FaJs className="icon" title="Javascript" />
-                <FaReact className="icon" title="ReactJS" />
-                <FaNode className="icon" title="NodeJS" />
-            </div>
-            <hr/>
-        </section>
-    )
-}
+      <div className='quote'>
+        <q>
+          The future belongs to those who learn more skills and combine them in
+          creative ways --- Robert Greene
+        </q>
+      </div>
+      <div className='skills-icons'>
+        <FaHtml5 className='icon' title='HTML5'></FaHtml5>
+        <FaCss3 className='icon' title='CSS3' />
+        <FaJs className='icon' title='Javascript' />
+        <FaReact className='icon' title='ReactJS' />
+        <FaNode className='icon' title='NodeJS' />
+      </div>
+      <hr />
+    </section>
+  );
+};
 
 export default Skills;
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,106 +1,124 @@
-
 const projects = [
-    {
-        id: 10,
-        title: "Live Chat",
-        img: "LiveChat.png",
-        description: "Users can chat with real time in the same room",
-        type: [
-            "https://i.ibb.co/g36pFxh/react.png",
-            "https://i.ibb.co/QDjPf2k/nodejs.png",
-            "https://i.ibb.co/yPQTsrH/express.png",
-            "https://i.ibb.co/gVgWfWR/socketio.png"
-        ],
-        github: "https://github.com/khoadodk/chat-app",
-        demo: "https://chat-khoado.netlify.com/"
-    },
-    {
-        id: 9,
-        title: "K Clothing",
-        img: "kclothing.png",
-        description: "An E-Commere store, user can add/edit/delete cart and process payment with stripe",
-        type: [
-            "https://i.ibb.co/g36pFxh/react.png",
-            "https://i.ibb.co/sW7zvSW/redux.png",
-            "https://i.ibb.co/5Lg6mhj/sass.jpg",
-            "https://i.ibb.co/bgJWxrj/firebase.png"
-        ],
-        github: "https://github.com/khoadodk/k-clothing",
-        demo: "https://kclothing.netlify.com/"
-    },
-    {
-        id: 6,
-        title: "budget calculator",
-        img: "budgetcal.png",
-        description: "Users can edit, add, or delete the expense",
-        type: ["https://i.ibb.co/g36pFxh/react.png"],
-        github: "https://github.com/khoadodk/budget-calculator",
-        demo: "https://budgetcal.netlify.com/"
-    },
-    {
-        id: 7,
-        title: "employee info",
-        img: "CRUD.png",
-        description: "A CRUD app that lets user add, view, edit or delete the employee info",
-        type: ["https://i.ibb.co/QDjPf2k/nodejs.png","https://i.ibb.co/yPQTsrH/express.png"],
-        github: "https://github.com/khoadodk/CRUD-hbs",
-        demo: "https://agile-bastion-41233.herokuapp.com/employee"
-    },
-    {
-        id: 8,
-        title: "electrik store",
-        img: "electrikstore.png",
-        description: "A Simple E-Commerce Store that lets user filter categories",
-        type: ["https://i.ibb.co/g36pFxh/react.png"],
-        github: "https://github.com/khoadodk/tech-store-react",
-        demo: "https://techstore-react.netlify.com"
-    },
-    // {
-    //     id: 1,
-    //     title: "text to speech",
-    //     img: "texttospeech.png",
-    //     description: "A simple text to speech app using voicerss.org API",
-    //     type: "javascript.png",
-    //     github: "https://github.com/khoadodk/texttospeech",
-    //     demo:"https://texttospeech-khoado.netlify.com/"
-    // },
-    // {
-    //     id: 2,
-    //     title: "to do app",
-    //     img: "todojs.png",
-    //     description: "A to do app that allows users to add, edit or delete tasks",
-    //     type: "javascript.png",
-    //     github: "https://github.com/khoadodk/todolist-js",
-    //     demo:"https://todojs-khoado.netlify.com/"
-    // },
-    // {
-    //     id: 3,
-    //     title: "tic tac toe",
-    //     img: "tictactoe.png",
-    //     description: "A simple tic tac toe game",
-    //     // type: "javascript.png",
-    //     github: "https://github.com/khoadodk/tictactoe",
-    //     demo: "https://tictactoe-khoado.netlify.com/"
-    // },
-    // {
-    //     id: 4,
-    //     title: "weather app",
-    //     img: "weather.png",
-    //     description: "A weather app that shows temp, condition, zone. Toggle temp between C and F ",
-    //     type: "javascript.png",
-    //     github: "https://github.com/khoadodk/weather-app",
-    //     demo: "https://weather-khoado.netlify.com/"
-    // },
-    {
-        id: 5,
-        title: "pixabay image finder",
-        img: "pixabayimagefinder.png",
-        description: "Search images using pixabay API and select amount of images",
-        type: ["https://i.ibb.co/g36pFxh/react.png"],
-        github: "https://github.com/khoadodk/pixabay-image-finder",
-        demo: "https://pixabayimagefinder-khoado.netlify.com/"
-    },
-    
-]
+  {
+    id: 11,
+    title: 'eSurvey',
+    img: 'esurvey.png',
+    description:
+      'Users send emails to their customers and collect the feedback',
+    type: [
+      'https://i.ibb.co/g36pFxh/react.png',
+      'https://i.ibb.co/QDjPf2k/nodejs.png',
+      'https://i.ibb.co/yPQTsrH/express.png',
+      'https://i.ibb.co/yftp6XF/mongodb.png'
+    ],
+    github: 'https://github.com/khoadodk/esurvey',
+    demo: 'https://pure-harbor-76603.herokuapp.com/'
+  },
+  {
+    id: 10,
+    title: 'Live Chat',
+    img: 'LiveChat.png',
+    description: 'Users can chat with real time in the same room',
+    type: [
+      'https://i.ibb.co/g36pFxh/react.png',
+      'https://i.ibb.co/QDjPf2k/nodejs.png',
+      'https://i.ibb.co/yPQTsrH/express.png',
+      'https://i.ibb.co/gVgWfWR/socketio.png'
+    ],
+    github: 'https://github.com/khoadodk/chat-app',
+    demo: 'https://chat-khoado.netlify.com/'
+  },
+  {
+    id: 9,
+    title: 'K Clothing',
+    img: 'kclothing.png',
+    description:
+      'An E-Commere store, user can add/edit/delete cart and process payment with stripe',
+    type: [
+      'https://i.ibb.co/g36pFxh/react.png',
+      'https://i.ibb.co/sW7zvSW/redux.png',
+      'https://i.ibb.co/5Lg6mhj/sass.jpg',
+      'https://i.ibb.co/bgJWxrj/firebase.png'
+    ],
+    github: 'https://github.com/khoadodk/k-clothing',
+    demo: 'https://kclothing.netlify.com/'
+  },
+  {
+    id: 6,
+    title: 'budget calculator',
+    img: 'budgetcal.png',
+    description: 'Users can edit, add, or delete the expense',
+    type: ['https://i.ibb.co/g36pFxh/react.png'],
+    github: 'https://github.com/khoadodk/budget-calculator',
+    demo: 'https://budgetcal.netlify.com/'
+  },
+  {
+    id: 7,
+    title: 'employee info',
+    img: 'CRUD.png',
+    description:
+      'A CRUD app that lets user add, view, edit or delete the employee info',
+    type: [
+      'https://i.ibb.co/QDjPf2k/nodejs.png',
+      'https://i.ibb.co/yPQTsrH/express.png'
+    ],
+    github: 'https://github.com/khoadodk/CRUD-hbs',
+    demo: 'https://agile-bastion-41233.herokuapp.com/employee'
+  },
+  {
+    id: 8,
+    title: 'electrik store',
+    img: 'electrikstore.png',
+    description: 'A Simple E-Commerce Store that lets user filter categories',
+    type: ['https://i.ibb.co/g36pFxh/react.png'],
+    github: 'https://github.com/khoadodk/tech-store-react',
+    demo: 'https://techstore-react.netlify.com'
+  },
+  // {
+  //     id: 1,
+  //     title: "text to speech",
+  //     img: "texttospeech.png",
+  //     description: "A simple text to speech app using voicerss.org API",
+  //     type: "javascript.png",
+  //     github: "https://github.com/khoadodk/texttospeech",
+  //     demo:"https://texttospeech-khoado.netlify.com/"
+  // },
+  // {
+  //     id: 2,
+  //     title: "to do app",
+  //     img: "todojs.png",
+  //     description: "A to do app that allows users to add, edit or delete tasks",
+  //     type: "javascript.png",
+  //     github: "https://github.com/khoadodk/todolist-js",
+  //     demo:"https://todojs-khoado.netlify.com/"
+  // },
+  // {
+  //     id: 3,
+  //     title: "tic tac toe",
+  //     img: "tictactoe.png",
+  //     description: "A simple tic tac toe game",
+  //     // type: "javascript.png",
+  //     github: "https://github.com/khoadodk/tictactoe",
+  //     demo: "https://tictactoe-khoado.netlify.com/"
+  // },
+  // {
+  //     id: 4,
+  //     title: "weather app",
+  //     img: "weather.png",
+  //     description: "A weather app that shows temp, condition, zone. Toggle temp between C and F ",
+  //     type: "javascript.png",
+  //     github: "https://github.com/khoadodk/weather-app",
+  //     demo: "https://weather-khoado.netlify.com/"
+  // },
+  {
+    id: 5,
+    title: 'pixabay image finder',
+    img: 'pixabayimagefinder.png',
+    description: 'Search images using pixabay API and select amount of images',
+    type: ['https://i.ibb.co/g36pFxh/react.png'],
+    github: 'https://github.com/khoadodk/pixabay-image-finder',
+    demo: 'https://pixabayimagefinder-khoado.netlify.com/'
+  }
+];
 
-export default projects
+export default projects;
